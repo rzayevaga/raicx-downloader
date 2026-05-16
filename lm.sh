75f5f8ae3835ec0cfe1ee96b531198a1f82640ae
Ağa Rzayev <rzayevaga.dev@gmail.com>
2026-05-16 22:53:41 +0400

    rxd-LM
    Signed-off-by: Ağa Rzayev <rzayevaga.dev@gmail.com>

--- lm.sh
+++ lm.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 set -o pipefail
 
-LM_VERSION="LM-V23.0"
+LM_VERSION="LM-V23.3"
 LM_DIR="$HOME/.raiclm"
 LM_CONFIG="$LM_DIR/lm.conf"
 LM_BIN="/data/data/com.termux/files/usr/bin/lm"
